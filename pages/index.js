bcd587834cfd407dda2ac33dda47292410603bfb
docokara <razavet.quentin37@gmail.com>
2022-11-19 00:46:47 +0100

    delete font

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,6 @@ import Projects from "./components/projects";
 export default function Home() {
   return (
     <div className={styles.home}>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap"
-        rel="stylesheet"
-      />
       <Entry />
       <AboutMe />
       <Parcours />
