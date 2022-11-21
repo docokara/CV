fdd1b968903a0404d32e5eb3d904bb420a2219da
docokara <razavet.quentin37@gmail.com>
2022-11-21 08:51:10 +0100

    about me

--- pages/components/aboutme/index.jsx
+++ pages/components/aboutme/index.jsx
@@ -4,10 +4,9 @@ import styles from "./aboutme.module.scss";
 export default function AboutMe({ children, title }) {
   return (
     <section id="aboutme-section" className={styles.aboutme}>
-      
       <p className={styles.fp}>
         {
-          "Passionnée d'informatique depuis toujours j'ai entreprit beaucoup de projet personnel pour me former. Mais depuis un an je me suis plus particulièrement intérréssé au côté web qui m'a énormément plus. Si je devais me décrire je dirais que je suis un acharné de travail et un éternel insatisfait."
+          "Passionnée d'informatique depuis toujours j'ai entrepris beaucoup de projet personnel pour me former. Mais depuis un an je me suis plus particulièrement intérréssé au côté web qui m'a énormément plus. Si je devais me décrire je dirais que je suis un acharné de travail          t."
         }
       </p>
     </section>
