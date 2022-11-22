d8b1b716ce98e3e387a3f16660a6913e34270c3a
docokara <razavet.quentin37@gmail.com>
2022-11-22 12:53:06 +0100

    update

--- pages/components/aboutme/index.jsx
+++ pages/components/aboutme/index.jsx
@@ -6,7 +6,7 @@ export default function AboutMe({ children, title }) {
     <section id="aboutme-section" className={styles.aboutme}>
       <p className={styles.fp}>
         {
-          "Passionnée d'informatique depuis toujours j'ai entrepris beaucoup de projet personnel pour me former. Mais depuis un an je me suis plus particulièrement intérréssé au côté web qui m'a énormément plus. Si je devais me décrire je dirais que je suis un acharné de travail          t."
+          "Passionné d'informatique depuis toujours, j'ai entrepris plusieurs de projets personnel pour me former. Mais depuis un an je me suis plus particulièrement intérréssé au côté web qui m'a énormément plus. Si je devais me décrire je dirais que je suis un acharné de travail et curieux."
         }
       </p>
     </section>
